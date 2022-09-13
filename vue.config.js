d5f54d8b99bb498e64607bfcea803c453a8206c5
pan <panw3i@gmail.com>
2022-09-13 13:55:05 +0800

    upgrade

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,31 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const path = require('path')
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
-const pages = {}
+const pages = {};
 
-const chromeName = ['popup', 'background', 'options', 'content']
+const chromeName = ["popup", "background", "options", "content"];
 
-chromeName.forEach(name => {
+chromeName.forEach((name) => {
   pages[name] = {
     entry: `src/pages/${name}/main.js`,
-    template: 'public/index.html',
-    filename: `${name}.html`
-  }
-})
+    template: "public/index.html",
+    filename: `${name}.html`,
+  };
+});
 
 module.exports = {
   pages,
   filenameHashing: false,
   configureWebpack: {
-    plugins: [CopyWebpackPlugin([{
-      from: path.resolve('manifest.json'),
-      to: `${path.resolve('dist')}/manifest.json`
-    }])]
-  }
-}
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve("manifest.json"),
+            to: `${path.resolve("dist")}/manifest.json`,
+          },
+        ],
+      }),
+    ],
+  },
+};
